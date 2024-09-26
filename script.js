a97f552819f02a5bc4095244b7d5d39a8b4ed9ae
Rahul Gupta <104988073+rahulg2527@users.noreply.github.com>
2024-09-26 13:25:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let searchbtn = document.querySelector('#search-btn');
+let searchbar = document.querySelector('.search-bar-container');
+let frombtn = document.querySelector('#login-btn');
+let loginform = document.querySelector(".login-form-container");
+let formclose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+let close=document.getElementById("close");
+
+
+windowscroll = () => {
+    Btn.classList.remove('fa-times');
+    searchbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginform.classList.remove('active');
+
+};
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchbtn.addEventListener('click', () => {
+    searchbtn.classList.toggle('fa-times');
+    searchbar.classList.toggle('active');
+    });
+
+frombtn.addEventListener('click', () => {
+    loginform.classList.add('active');
+});
+
+
+close.addEventListener('click', () => {
+    loginform.classList.remove('active');
+});
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+
+});
+
+var Swiper = new Swiper(".review-slider", {
+    spaceBetween:20,
+    loop:true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        640:{
+            slidesPreView: 1,
+        },
+        768:{
+            slidesPreView: 2,
+        },
+        1024:{
+            slidesPreView: 3,
+        },
+    },
+});
+
